b136627c4d3dc58c314039e0907987f388b8ae71
Domagoj Saric <dsaritz@gmail.com>
2016-11-07 12:24:36 +0100

    Improved the generic shop implementation:  - reduced contention on the main conditional variable (by replacing it with per-worker conditional variables)  - removed the use of a std::vector container for workers (since we don't need a resizeable container)  - added an experimental spin-lock based work-waiting optimization on Android. Minor other related refactoring and optimization.

--- include/boost/sweater/detail/generic.hpp
+++ include/boost/sweater/detail/generic.hpp
@@ -21,12 +21,14 @@
 #include <boost/container/static_vector.hpp>
 #include <boost/functionoid/functionoid.hpp>
 #include <boost/range/algorithm/count_if.hpp>
+#include <boost/range/iterator_range_core.hpp>
 
+#include <atomic>
 #include <cstdint>
 #include <future>
+#include <memory>
 #include <mutex>
 #include <thread>
-#include <vector>
 //------------------------------------------------------------------------------
 namespace boost
 {
@@ -44,46 +46,74 @@ inline auto hardware_concurency() noexcept { return static_cast<std::uint8_t>( s
 struct impl
 {
 	impl()
+#if BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
+    : pool_( BOOST_SWEATER_MAX_HARDWARE_CONCURENCY - 1 )
+#endif
 	{
-		auto const actual_threads( hardware_concurency() );
-		std::unique_lock<std::mutex> const my_lock( mutex_ );
-		for ( std::uint8_t t( 0U ); t < actual_threads - 1; ++t )
+    #if !BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
+        auto const number_of_worker_threads( hardware_concurency() - 1 );
+        auto p_workers( std::make_unique<worker[]>( number_of_worker_threads ) );
+        pool_ = make_iterator_range_n( p_workers.get(), number_of_worker_threads );
+    #endif // !BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
+		for ( auto & worker : pool_ )
 		{
 			auto const worker_loop
 			(
-				[t, this]() noexcept
+				[&worker, this]() noexcept
 				{
-					auto & __restrict the_mutex( mutex_ );
-					the_mutex.lock();
-					auto & __restrict my_work  ( pool_[ t ].first );
-					the_mutex.unlock();
+                    auto & __restrict my_work_available( worker.have_work );
+                    auto & __restrict my_work          ( worker.work  );
+                    auto & __restrict my_event         ( worker.event );
 
-					for ( ; ; )
-					{
-						{
-							std::unique_lock<std::mutex> my_lock( the_mutex );
-							while ( !my_work && BOOST_LIKELY( !brexit_ ) )
-								event_.wait( my_lock );
-						}
-						if ( BOOST_UNLIKELY( brexit_ ) )
+                    for ( ; ; )
+                    {
+                        if ( BOOST_UNLIKELY( brexit_.load( std::memory_order_relaxed ) ) )
 							return;
-                        my_work();
-                        std::unique_lock<std::mutex> my_lock( the_mutex );
-						my_work.clear();
-						event_.notify_all();
-					}
+
+                    #ifdef __ANROID__
+                        // https://petewarden.com/2015/10/11/one-weird-trick-for-faster-android-multithreading
+                        auto constexpr spin_count( 30 * 1000 * 1000 );
+                    #else
+                        auto constexpr spin_count(                1 );
+                    #endif // __ANROID__
+
+                        for ( auto try_count( 0 ); try_count < spin_count; ++try_count )
+                        {
+                            if ( BOOST_LIKELY( my_work_available.load( std::memory_order_acquire ) ) )
+                            {
+                                my_work();
+                                my_work.clear();
+                                /// \note The
+                                /// std::condition_variable::notify_one() call
+                                /// below should imply a release so we can get
+                                /// away with a relaxed store here.
+                                ///           (14.10.2016.) (Domagoj Saric)
+                                my_work_available.store( false, std::memory_order_relaxed );
+                                my_event.notify_one();
+                            }
+                        }
+                        worker.wait( mutex_ );
+                    }
 				}
-			);
-			pool_.emplace_back( std::piecewise_construct, std::make_tuple(), std::make_tuple( worker_loop ) );
+			); // worker_loop
+            worker.thread = std::thread( worker_loop );
 		}
+    #if !BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
+        p_workers.release();
+    #endif // !BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
 	}
 
 	~impl() noexcept
 	{
-		brexit_ = true;
-		event_.notify_all();
+		brexit_.store( true, std::memory_order_relaxed );
 		for ( auto & worker : pool_ )
-			worker.second.join();
+        {
+            worker.event .notify_one();
+			worker.thread.join      ();
+        }
+    #if !BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
+        delete[] pool_.begin();
+    #endif // BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
 	}
 
 	auto number_of_workers() const { return static_cast<std::uint16_t>( pool_.size() + 1 ); }
@@ -111,7 +141,6 @@ struct impl
 			iteration, iterations_per_worker,
 			work
 		);
-		event_.notify_all();
 
 		auto const caller_thread_start_iteration( iteration );
 		BOOST_ASSERT( caller_thread_start_iteration <= iterations );
@@ -136,9 +165,11 @@ struct impl
 private:
 	void join() noexcept
 	{
-		std::unique_lock<std::mutex> my_lock( mutex_ );
-		while ( boost::count_if( pool_, []( auto const & worker ) { return !worker.first.empty(); } ) )
-			event_.wait( my_lock );
+        for ( auto & worker : pool_ )
+        {
+            while ( worker.have_work.load( std::memory_order_acquire ) )
+                worker.wait( mutex_ );
+        }
 	}
 
     template <typename F>
@@ -154,56 +185,61 @@ private:
 		auto iteration( begin_iteration );
 		for ( auto thread_index( begin_thread ); thread_index < end_thread; ++thread_index )
 		{
-			auto & delegate( pool_[ thread_index ].first );
+			auto & delegate( pool_[ thread_index ].work );
             auto const end_iteration( iteration + iterations_per_worker );
             delegate = [&work, start_iteration = iteration, end_iteration]() noexcept
             {
 				work( start_iteration, end_iteration );
             };
+            pool_[ thread_index ].event.notify_one();
 			iteration = end_iteration;
 		}
 		return iteration;
 	}
 
 private:
-	bool volatile           brexit_ = false;
-	std::mutex              mutex_;
-	std::condition_variable event_;
+    std::atomic<bool> brexit_ = ATOMIC_FLAG_INIT;
+	std::mutex        mutex_;
 
     struct worker_traits : functionoid::std_traits
     {
-        static constexpr auto copyable             = functionoid::support_level::na    ;
-        static constexpr auto moveable             = functionoid::support_level::nofail;
-        static constexpr auto destructor           = functionoid::support_level::nofail;
-        static constexpr auto is_noexcept          = true;
-        static constexpr auto rtti                 = false;
+        static constexpr auto copyable    = functionoid::support_level::na    ;
+        static constexpr auto moveable    = functionoid::support_level::nofail;
+        static constexpr auto destructor  = functionoid::support_level::nofail;
+        static constexpr auto is_noexcept = true;
+        static constexpr auto rtti        = false;
 
-        static constexpr std::uint8_t sbo_size = 4 * sizeof( void * );
-        static constexpr std::uint8_t sbo_alignment = alignof( std::max_align_t );
-
-        using empty_handler = functionoid::nop_on_empty;
+        using empty_handler = functionoid::assert_on_empty;
     }; // struct worker_traits
 
-    using worker = std::pair
-	<
-		functionoid::callable<void(), worker_traits>,
-		std::thread
-	>;
+    struct worker
+    {
+        std::atomic<bool>                            have_work = ATOMIC_FLAG_INIT;
+        functionoid::callable<void(), worker_traits> work  ;
+        std::condition_variable                      event ;
+        std::thread                                  thread;
+
+        BOOST_NOINLINE
+        void wait( std::mutex & mutex ) noexcept { event.wait( std::unique_lock<std::mutex>( mutex ) ); }
+	};
 #if BOOST_SWEATER_MAX_HARDWARE_CONCURENCY
 	using pool_threads_t = container::static_vector<worker, BOOST_SWEATER_MAX_HARDWARE_CONCURENCY - 1>; // also sweat on the calling thread
 #else
-	using pool_threads_t = std::vector<worker>;
+    using pool_threads_t = iterator_range<worker *>;
 #endif
 	pool_threads_t pool_;
 
-    /// \note .
+    /// \todo Implement a work queue.
     /// https://en.wikipedia.org/wiki/Work_stealing
+    /// http://www.drdobbs.com/parallel/writing-lock-free-code-a-corrected-queue/210604448
     /// https://github.com/cameron314/readerwriterqueue
+    /// http://moodycamel.com/blog/2013/a-fast-lock-free-queue-for-c++
+    /// http://stackoverflow.com/questions/1164023/is-there-a-production-ready-lock-free-queue-or-hash-implementation-in-c#14936831
     /// https://github.com/facebook/folly/blob/master/folly/docs/ProducerConsumerQueue.md
     /// https://github.com/facebook/folly/blob/master/folly/MPMCQueue.h
     /// http://landenlabs.com/code/ring/ring.html
     /// https://github.com/Qarterd/Honeycomb/blob/master/src/common/Honey/Thread/Pool.cpp
-    ///                                           (12.10.2016.) (Domagoj Saric)
+    ///                                       (12.10.2016.) (Domagoj Saric)
 }; // struct impl
 
 //------------------------------------------------------------------------------
