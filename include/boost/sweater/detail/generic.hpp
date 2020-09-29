f62edf52fcf272922d7a50eb3df64a71d11325bb
Luka Markušić <luka.markusic@microblink.com>
2020-09-29 13:44:49 +0200

    Add the right clang UBSAN suppression

--- include/boost/sweater/detail/generic.hpp
+++ include/boost/sweater/detail/generic.hpp
@@ -1001,9 +1001,7 @@ private:
 #   if BOOST_SWEATER_USE_CALLER_THREAD
         BOOST_NOINLINE void spin_wait() noexcept
 #       ifdef __clang__
-          __attribute__(( no_sanitize( "implicit-integer-truncation"  ) ))
-          __attribute__(( no_sanitize( "implicit-integer-sign-change" ) ))
-          __attribute__(( no_sanitize( "undefined"                    ) ))
+          __attribute__(( no_sanitize( "unsigned-integer-overflow" ) ))
 #       endif
         {
             BOOST_ASSERT( spin_wait_ );
