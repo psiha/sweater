5f701aee1f558c23bbd8c80527bd2f44281d3ac9
Domagoj Saric <dsaritz@gmail.com>
2025-11-14 13:40:38 +0100

    Made the various mutex types 'copyable-while-dormant'.

--- include/boost/sweater/threading/windows/mutex.hpp
+++ include/boost/sweater/threading/windows/mutex.hpp
@@ -3,7 +3,7 @@
 /// \file mutex.hpp
 /// ---------------
 ///
-/// (c) Copyright Domagoj Saric 2016 - 2021.
+/// (c) Copyright Domagoj Saric 2016 - 2026.
 ///
 ///  Use, modification and distribution are subject to the
 ///  Boost Software License, Version 1.0. (See accompanying file
@@ -17,10 +17,7 @@
 //------------------------------------------------------------------------------
 #include <windows.h>
 //------------------------------------------------------------------------------
-namespace boost
-{
-//------------------------------------------------------------------------------
-namespace thrd_lite
+namespace boost::thrd_lite
 {
 //------------------------------------------------------------------------------
 
@@ -29,10 +26,11 @@ class win32_condition_variable;
 class win32_slim_mutex
 {
 public:
-    win32_slim_mutex(                           ) noexcept : lock_( SRWLOCK_INIT ) {}
-    win32_slim_mutex( win32_slim_mutex && other ) noexcept : lock_{ other.lock_ } { other.lock_ = SRWLOCK_INIT; }
-    win32_slim_mutex( win32_slim_mutex const &  ) = delete ;
-   ~win32_slim_mutex(                           ) = default;
+    win32_slim_mutex() = default;
+   ~win32_slim_mutex() = default;
+    explicit // allow copy so as to enable use compiler generated constructors/functions for types that contain mutex members
+    win32_slim_mutex( [[ maybe_unused ]] win32_slim_mutex const &  other ) noexcept { BOOST_VERIFY_MSG( !other.lock_.Ptr, "Copy allowed only for dormant mutexes" ); }
+    win32_slim_mutex( [[ maybe_unused ]] win32_slim_mutex       && other ) noexcept { BOOST_VERIFY_MSG( !other.lock_.Ptr, "Relocation allowed only for dormant mutexes" ); }
 
     void   lock() noexcept { ::AcquireSRWLockExclusive( &lock_ ); }
     void unlock() noexcept { ::ReleaseSRWLockExclusive( &lock_ ); }
@@ -40,11 +38,9 @@ public:
     bool try_lock() noexcept { return ::TryAcquireSRWLockExclusive( &lock_ ) != false; }
 
 private: friend class win32_condition_variable;
-    ::SRWLOCK lock_;
+    ::SRWLOCK lock_ = SRWLOCK_INIT;
 }; // class win32_slim_mutex
 
 //------------------------------------------------------------------------------
-} // namespace thrd_lite
-//------------------------------------------------------------------------------
-} // namespace boost
+} // namespace boost::thrd_lite
 //------------------------------------------------------------------------------
